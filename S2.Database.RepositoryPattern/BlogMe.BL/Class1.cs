ab45268febd486381f3d87d59d77c00e95f013de
Daniel Valsby-Koch <dava@aspit.dk>
2019-03-31 19:51:52 +0200

    Added repository example Added courtbooking example

---
+++ S2.Database.RepositoryPattern/BlogMe.BL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BlogMe.BL
+{
+    public class Class1
+    {
+    }
+}
---
+++ S2.Database.RepositoryPattern/BlogMe.Entities/BlogPost.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace BlogMe.Entities
+{
+    [Table("BlogPost")]
+    public class BlogPost : IEntity
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string PostContent { get; set; }
+
+    }
+}
